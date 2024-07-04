2f0e82661dd6f466d9b34a4d894d6b932ca7ba22
YomEngine <yominzoku@gmail.com>
2024-07-04 12:20:26 +0900

    Initial Commit

---
+++ TimeChecker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TimeChecker
+//
+//  Created by yotahara on 2024/07/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TimeChecker/TimeCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeCheckerApp.swift
+//  TimeChecker
+//
+//  Created by yotahara on 2024/07/04.
+//
+
+import SwiftUI
+
+@main
+struct TimeCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
